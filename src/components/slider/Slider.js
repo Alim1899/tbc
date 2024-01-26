4cebd4f2bb8073a017be10648ca1bf0c4312e69c
Alim1899 <alimakhchanogli@gmail.com>
2024-01-27 01:28:46 +0400

    Added slider component with little bugs

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -5,17 +5,116 @@ import tnetLogo from "../../assets/sliderImages/tnet.svg";
 import spectreLogo from "../../assets/sliderImages/spectre.svg";
 import usaidLogo from "../../assets/sliderImages/usaid.svg";
 import tbcLeasingLogo from "../../assets/sliderImages/tbcleasing.svg";
+import leftSlide from "../../assets/icons/leftslide.svg";
+import rightSlide from "../../assets/icons/rightslide.svg";
 
+import classes from "./Slider.module.css";
+import { useState, useEffect, useCallback, useRef } from "react";
 const Slider = () => {
+  const [showFirstSlide, setShowFirstSlide] = useState(true);
+  const [showSecondSlide, setShowSecondSlide] = useState(false);
+  const [showThirdSlide, setShowThirdSlide] = useState(false);
+  const [activeSlide, setActiveSlide] = useState(1);
+
+  const slider = useCallback(() => {
+    if (activeSlide === 1) {
+      setShowFirstSlide(true);
+      setShowSecondSlide(false);
+      setShowThirdSlide(false);
+    } else if (activeSlide === 2) {
+      setShowFirstSlide(false);
+      setShowSecondSlide(true);
+      setShowThirdSlide(false);
+    } else if (activeSlide === 3) {
+      setShowFirstSlide(false);
+      setShowSecondSlide(false);
+      setShowThirdSlide(true);
+    } else {
+      if (activeSlide >= 4) {
+        setActiveSlide(1);
+      } else if (activeSlide <= 0) {
+        setActiveSlide(3);
+      }
+    }
+  }, [activeSlide]);
+
+  useEffect(() => {
+    slider();
+  }, [slider]);
+  setTimeout(() => {
+    setActiveSlide(activeSlide+1)
+  }, 3500);
+  
+
+  const changeByButtons = (e) => {
+    if (e.target.id === "left") {
+      setActiveSlide(activeSlide - 1);
+    } else if (e.target.id === "right") {
+      setActiveSlide(activeSlide + 1);
+    }
+  };
+
   return (
-    <div>
-      <img src={ufcLogoSvg} alt="ufclogo"></img>
-      <img src={tegetaLogo} alt="ufclogo"></img>
-      <img src={spaceLogo} alt="ufclogo"></img>
-      <img src={tnetLogo} alt="tnet"></img>
-      <img src={spectreLogo} alt="tnet"></img>
-      <img src={usaidLogo} alt="tnet"></img>
-      <img src={tbcLeasingLogo} alt="tnet"></img>
+    <div className={classes.main}>
+      <img
+        alt="leftArrow"
+        id="left"
+        onClick={changeByButtons}
+        className={classes.leftArrow}
+        src={leftSlide}
+      ></img>
+      <div className={classes.content}>
+        <h1 className={classes.head}>პროექტის პარტნიორები</h1>
+        {showFirstSlide && (
+          <div className={classes.firstSlide}>
+            <div>
+              <img src={usaidLogo} alt="tnet"></img>
+            </div>
+            <div>
+              {" "}
+              <img src={spaceLogo} alt="ufclogo"></img>
+            </div>
+            <div>
+              <img src={tnetLogo} alt="tnet"></img>
+            </div>
+          </div>
+        )}
+        {showSecondSlide && (
+          <div className={classes.secondSlide}>
+            <div>
+              <img src={tegetaLogo} alt="ufclogo"></img>
+            </div>
+            <div>
+              {" "}
+              <img src={spectreLogo} alt="tnet"></img>
+            </div>
+            <div>
+              <img src={tbcLeasingLogo} alt="tnet"></img>
+            </div>
+          </div>
+        )}
+        {showThirdSlide && (
+          <div className={classes.thirdSlide}>
+            <div></div>
+            <div>
+              <img src={ufcLogoSvg} alt="ufclogo"></img>
+            </div>
+            <div></div>
+          </div>
+        )}
+        <ul className={classes.dots}>
+          <li onClick={() => setActiveSlide(1)} id="1"></li>
+          <li onClick={() => setActiveSlide(2)} id="2"></li>
+          <li onClick={() => setActiveSlide(3)} id="3"></li>
+        </ul>
+      </div>
+      <img
+        onClick={changeByButtons}
+        id="right"
+        alt="rightArrow"
+        className={classes.rightArrow}
+        src={rightSlide}
+      ></img>
     </div>
   );
 };
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -3,14 +3,16 @@ import Navbar from "../components/navbar/Navbar";
 import Header from "../components/header/Header";
 import About from "../components/about/About";
 import Courses from "../components/courses/Courses";
+import Slider from "../components/slider/Slider";
 import FAQ from "../components/faq/FAQ";
 const Layout = () => {
   return (
     <div className={classes.layout}>
-      <Navbar/>
-      <Header/>
-      <About/>
-      <Courses/>
+    <Navbar/>
+    <Header/>
+    <About/>
+    <Courses/>
+      <Slider/>
       <FAQ/>
     </div>
   );
